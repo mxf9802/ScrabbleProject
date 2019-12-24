100b21e707e2b597842aa10ea2a166c77dc2d598
Mohsin <mxf9802@gmail.com>
2019-12-24 00:03:57 -0800

    Changed the LetterBag implementation to not be stupid and just use a shuffle.

--- src/main/java/Mechanics/LetterBag.java
+++ src/main/java/Mechanics/LetterBag.java
@@ -1,106 +1,90 @@
 package Mechanics;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 /**
  * Created by Mohsin on 12/8/2019.
  */
 public class LetterBag {
 
-    private int[] letterBag;
+    private List<Character> allLetters;
+    private int currIndex;
 
     private Random randomNumberGenerator;
 
     private boolean isBagEmpty;
 
     public LetterBag() {
-        letterBag = new int[26];
+        isBagEmpty = false;
+        allLetters = new ArrayList<>();
         initLetterBag();
         randomNumberGenerator = new Random();
     }
 
     private void initLetterBag() {
-        letterBag[0] = 9;
-        letterBag[1] = 2;
-        letterBag[2] = 2;
-        letterBag[3] = 4;
-        letterBag[4] = 12;
-        letterBag[5] = 2;
-        letterBag[6] = 3;
-        letterBag[7] = 2;
-        letterBag[8] = 9;
-        letterBag[9] = 1;
-        letterBag[10] =1;
-        letterBag[11] = 4;
-        letterBag[12] = 2;
-        letterBag[13] = 6;
-        letterBag[14] = 8;
-        letterBag[15] = 2;
-        letterBag[16] = 1;
-        letterBag[17] = 6;
-        letterBag[18] = 4;
-        letterBag[19] = 6;
-        letterBag[20] = 4;
-        letterBag[21] = 2;
-        letterBag[22] = 2;
-        letterBag[23] = 1;
-        letterBag[24] = 2;
-        letterBag[25] = 1;
+        IntStream.range(0,9).forEach(val -> allLetters.add('A'));
+        IntStream.range(0,2).forEach(val -> allLetters.add('B'));
+        IntStream.range(0,2).forEach(val -> allLetters.add('C'));
+        IntStream.range(0,4).forEach(val -> allLetters.add('D'));
+        IntStream.range(0,12).forEach(val -> allLetters.add('E'));
+        IntStream.range(0,2).forEach(val -> allLetters.add('F'));
+        IntStream.range(0,3).forEach(val -> allLetters.add('G'));
+        IntStream.range(0,2).forEach(val -> allLetters.add('H'));
+        IntStream.range(0,9).forEach(val -> allLetters.add('I'));
+        IntStream.range(0,1).forEach(val -> allLetters.add('J'));
+        IntStream.range(0,1).forEach(val -> allLetters.add('K'));
+        IntStream.range(0,4).forEach(val -> allLetters.add('L'));
+        IntStream.range(0,2).forEach(val -> allLetters.add('M'));
+        IntStream.range(0,6).forEach(val -> allLetters.add('N'));
+        IntStream.range(0,8).forEach(val -> allLetters.add('O'));
+        IntStream.range(0,2).forEach(val -> allLetters.add('P'));
+        IntStream.range(0,1).forEach(val -> allLetters.add('Q'));
+        IntStream.range(0,6).forEach(val -> allLetters.add('R'));
+        IntStream.range(0,4).forEach(val -> allLetters.add('S'));
+        IntStream.range(0,6).forEach(val -> allLetters.add('T'));
+        IntStream.range(0,4).forEach(val -> allLetters.add('U'));
+        IntStream.range(0,2).forEach(val -> allLetters.add('V'));
+        IntStream.range(0,2).forEach(val -> allLetters.add('W'));
+        IntStream.range(0,1).forEach(val -> allLetters.add('X'));
+        IntStream.range(0,2).forEach(val -> allLetters.add('Y'));
+        IntStream.range(0,1).forEach(val -> allLetters.add('Z'));
 
+        currIndex = allLetters.size()-1;
+        IntStream.range(0,2).forEach(val -> shuffle(allLetters));
     }
 
+    private void shuffle(List<Character> allLetters) {
+        int randomIndex;
+        Character tempval;
+        for (int i=0; i < allLetters.size(); i++) {
+            randomIndex = randomNumberGenerator.nextInt(allLetters.size());
+            tempval = allLetters.get(i);
+            allLetters.set(i, allLetters.get(randomIndex));
+            allLetters.set(randomIndex, tempval);
+        }
+    }
+
+    public boolean isBagEmpty() {
+        return isBagEmpty;
+    }
     public List<Character> getRandomChars(int numCount) {
-        List<Character> results = new ArrayList<>();
-        int totalCharCount = 0;
 
         if (isBagEmpty) {
-            return results;
+            return new ArrayList<>();
+        } else if(currIndex <= 0) {
+            isBagEmpty = true;
+            return new ArrayList<>();
         }
 
-        for (int i=0; i < letterBag.length; i++) {
-            totalCharCount += letterBag[i];
+        List<Character> results = new ArrayList<>();
+        while(currIndex > 0 && numCount > 0) {
+            results.add(allLetters.get(currIndex));
+            currIndex--;
+            numCount--;
         }
 
-        if (totalCharCount == 0) {
-            isBagEmpty = false;
-            return results;
-        }
-
-        if (numCount > totalCharCount) {
-            for (int i=0; i < letterBag.length; i++) {
-                for(int j=0; j<letterBag[i]; j++) {
-                    results.add(convertChar(i));
-                }
-                letterBag[i] = 0;
-            }
-            return results;
-        }
-        for (int i=0; i<numCount; i++) {
-            int randomIndex = randomNumberGenerator.nextInt(totalCharCount);
-            results.add(getRandomChar(randomIndex));
-            totalCharCount--;
-        }
         return results;
     }
-
-    private Character getRandomChar(final int randomIndex) {
-        int currCount = 0;
-        for (int i=0; i < letterBag.length; i++) {
-            for (int j=0; j < letterBag[i]; j++) {
-                if(randomIndex == currCount) {
-                    letterBag[i]--;
-                    return convertChar(i);
-                }
-                currCount++;
-            }
-        }
-        throw new IllegalStateException("Unable to find the character in the bag for index:" + randomIndex);
-    }
-
-    private char convertChar(int val) {
-        return (char)('A' + val);
-    }
-
 }
